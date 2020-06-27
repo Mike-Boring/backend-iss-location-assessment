4e4254484173b56bf3f8e30402eb9bfcbd783efc
Mike Boring <shekinah999@msn.com>
2020-06-27 12:55:07 -0600

    Part a Completed

--- iss.py
+++ iss.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python
 
-__author__ = '???'
+__author__ = 'Mike Boring'
+
+import requests
 
 
 def main():
-    pass
+    req = requests.get(
+        'http://api.open-notify.org/astros.json')
+    converted_content = req.json()  # convert json
+    people_list = converted_content['people']  # list of people
+    print('Number of Astronauts in Space is:', len(people_list))
+    print('Current Astronauts in Space:')
+    for person in people_list:
+        print('Name: ', person['name'])
+        print('Spacecraft: ', person['craft'])
 
 
 if __name__ == '__main__':
